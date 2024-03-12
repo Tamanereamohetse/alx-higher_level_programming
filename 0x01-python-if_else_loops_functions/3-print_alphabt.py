b8c1c513265bdfeb153d18a646ecca69806b0f61
Tamanereamohetse <tamanereamohetse@gmail.com>
2024-03-12 05:55:54 -0700

    TASK3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for a in range(97, 123):
-    if a in [101, 113]:
-        print("{}".format(chr(a)), end='')
+for i in range(ord('a'), ord('z') + 1):
+    if chr(i) != 'e' and chr(i) != 'q':
+        print('{:c}'.format(i), end="")
